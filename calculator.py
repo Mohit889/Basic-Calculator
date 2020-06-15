4e8cb95520f507ab5a235137e9d57e3fff3aee03
Mohit Patel <51730944+Mohit889@users.noreply.github.com>
2020-06-15 11:38:23 +0530

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -8,6 +8,16 @@ fin_ans = tkinter.StringVar()
 ans = None
 operator = ""   #string for knowing about the opted opeartor
 
+def set_text(num):
+    current = n11.focus_displayof()
+
+    number = str(current.get())
+    number_of_digits = len(number)
+    current.delete(0,number_of_digits)
+    number=number +str(num)
+    current.insert(number_of_digits, number)
+    #n11.insert(number_of_digits,number)
+
 
 def set_operator(text):
     global operator
@@ -16,10 +26,11 @@ def set_operator(text):
 def submit():
     number1=n11.get()
     number2=n22.get()
+    global ans
     try:
         first = int(number1)
         second = int(number2)
-        global ans
+
         global operator
         if operator == "add":
             ans = first + second
@@ -47,6 +58,16 @@ n2 = tkinter.Label(root,text="Enter second number")
 n_ans = tkinter.Label(root,text="Answer:")
 n11 = tkinter.Entry(root, textvariable=first_number)
 n22 = tkinter.Entry(root, textvariable=second_number)
+b1 = tkinter.Button(root,text="1",width=7,command=lambda: (set_text(1)))
+b2 = tkinter.Button(root,text="2",width=7,command=lambda: (set_text(2)))
+b3 = tkinter.Button(root,text="3",width=7,command=lambda: (set_text(3)))
+b4 = tkinter.Button(root,text="4",width=7,command=lambda: (set_text(4)))
+b5 = tkinter.Button(root,text="5",width=7,command=lambda: (set_text(5)))
+b6 = tkinter.Button(root,text="6",width=7,command=lambda: (set_text(6)))
+b7 = tkinter.Button(root,text="7",width=7,command=lambda: (set_text(7)))
+b8 = tkinter.Button(root,text="8",width=7,command=lambda: (set_text(8)))
+b9 = tkinter.Button(root,text="9",width=7,command=lambda: (set_text(9)))
+b0 = tkinter.Button(root,text="0",width=7,command=lambda: (set_text(0)))
 n_ans1 = tkinter.Label(root, textvariable=fin_ans)
 add = tkinter.Button(root,text="+",width=10,command=lambda: (set_operator("add")))
 subtract = tkinter.Button(root,text="-",width=10,command=lambda: (set_operator("subtract")))
@@ -64,4 +85,14 @@ multiply.grid(row=3,column=0)
 divide.grid(row=3,column=1)
 n_ans.grid(row=5,column=0)
 n_ans1.grid(row=5, column=1)
+b1.grid(row=6,column=0)
+b2.grid(row=6,column=1)
+b3.grid(row=6,column=2)
+b4.grid(row=7,column=0)
+b5.grid(row=7,column=1)
+b6.grid(row=7,column=2)
+b7.grid(row=8,column=0)
+b8.grid(row=8,column=1)
+b9.grid(row=8,column=2)
+b0.grid(row=9,column=0)
 root.mainloop()
